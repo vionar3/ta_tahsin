132bef1084e04816963b714e9f8331390bf85ec7
vionar3 <irfandimasriyadi@gmail.com>
2025-07-24 13:38:05 +0700

    edit icon name apk & base url

--- lib/core/baseurl/base_url.dart
+++ lib/core/baseurl/base_url.dart
@@ -1,5 +1,5 @@
 // lib/config.dart
 class BaseUrl {
-  static const String baseUrl = 'https://legal-marginally-macaque.ngrok-free.app/api';
-  static const String audioUrl = 'https://legal-marginally-macaque.ngrok-free.app';
+  static const String baseUrl = 'https://api-tahsin-production.up.railway.app/api';
+  static const String audioUrl = 'https://api-tahsin-production.up.railway.app';
 }
